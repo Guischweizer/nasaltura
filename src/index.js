155b74ae8f43d1097394670e60dc4d694d3d12a9
guilherme <guilherme.schweizer@gmail.com>
2018-11-30 12:02:01 -0200

    Created initial layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+import "./Content/Stylesheet/StyleFull.scss"
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -9,4 +13,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();

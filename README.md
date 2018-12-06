df01d0c3006b53ac3e9c85978d198604b0efbda4
guilherme <guilherme.schweizer@gmail.com>
2018-12-06 20:03:16 -0200

    Changes in README and make some changes in Full.jsx

--- README.md
+++ README.md
@@ -1,23 +1,32 @@
-So far we just have a simple dashboard layout structure.
-So if you want start this project all you gonna need to do is:
+So far we just have a simple dashboard layout structure.  
+So if you want start this project all you gonna need to do is:  
 
 
 **You gonna need a API KEY to make NASA Requests so, you can get one in this link:**
 > https://api.nasa.gov/index.html#apply-for-an-api-key
 
-After you have a API KEY, we need to place that one in our `globalconfig.jsx` file and change the value of the API_KEY.
+After you have a API KEY, we need to place that one in our `globalconfig.jsx` file and change the value of the API_KEY.  
 
 
-**First:**
+***IMPORTANT**  
+We have thre importants branchs on our project:  
+
+master -> That's is the production build, that branch have the LTS version of the project.  
+dev -> That's the development branch, here we can play and develop a lot of thinks cool.  
+homolog -> Here we gonna do some tests before the merge with the master branch.  
+
+
+
+**First:**  
 Clone the project with: 
 > ´git clone https://github.com/Guischweizer/nasaltura.git´
 
-The second step is get into the folder
-> ´cd nasaltura´
+The second step is get into the folder  
+> ´cd nasaltura´  
  and run 
-> ´npm install´
+> ´npm install´  
 
-After all you need to do is run the project with 
->´npm start´ 
-and access your 
->´localhost:3000´
+After all you need to do is run the project with   
+>´npm start´   
+and access your   
+>´localhost:3000´  
--- src/Layout/Full.jsx
+++ src/Layout/Full.jsx
@@ -2,40 +2,25 @@ import React, { Component } from 'react';
 import API_Requisition from '../API_Requisition/Asteroids_API';
 
 class Full extends Component {
-  componentDidMount(){
-    API_Requisition();
+  componentDidMount() {
+    // API_Requisition();
   }
   render() {
     return (
-    <div>
-       <section id="sideMenu">
-  <nav>
-    <a href="*" className="active"><i className="fa fa-home" aria-hidden="true"></i>
- Home</a>
-    <a href="*"><i className="fa fa-sticky-note-o" aria-hidden="true"></i> What's New</a>
-    <a href="*"><i className="fa fa-bookmark-o" aria-hidden="true"></i> Get Started</a>
-    <a href="*"><i className="fa fa-calendar-check-o" aria-hidden="true"></i> Accessiblity</a>
-    <a href="*"><i className="fa fa-user-circle-o" aria-hidden="true"></i> Community</a>
-    <a href="*"><i className="fa fa-cog" aria-hidden="true"></i> License</a>
+      <div>
+        <section id="sideMenu">
+          <nav>
+          <a href="*" ><i className="fa fa-cog" aria-hidden="true"></i> EPIC API REQUISITION</a>
   </nav>
 </section>
 
-<header>
-  <div className="search-area">
-    <i className="fa fa-search" aria-hidden="true"></i>
-    <input type="text"/>
-  </div>
-  <div className="user-area">
-    <a href="*">+ Add</a>
-    <a href="*" className="notification"><i className="fa fa-bell-o" aria-hidden="true"></i>
- <span className="circle">3</span></a>
-    <a href="*">
-      <div className="user-img"></div>
-      <i className="fa fa-caret-down" aria-hidden="true"></i>
-    </a>
-  </div>
-</header>
-</div>
+      <header>
+        <div className="search-area">
+          <i className="fa fa-search" aria-hidden="true"></i>
+          <input type="text" />
+        </div>
+      </header>
+</div >
     );
   }
 }
